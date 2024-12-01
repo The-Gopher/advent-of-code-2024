d63b9ee700d19c04846f5be9224d7c05590f1759
Mike Franklin <mfranklin@sampleserve.com>
2024-12-01 17:01:53 -0600

    Day 1 - Step 2

--- 1/main.py
+++ 1/main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+from collections import Counter
 
 
-def main():
+def step1():
     input = Path(__file__).parent / "input"
 
     lines = input.read_text().splitlines()
@@ -9,9 +10,23 @@ def main():
     list_b = sorted([int(x.split("   ")[1]) for x in lines])
     diff = [abs(x - y) for x, y in zip(list_a, list_b)]
     sum_diff = sum(diff)
-    
+
     print(sum_diff)
 
 
+def step2():
+    input = Path(__file__).parent / "input"
+
+    lines = input.read_text().splitlines()
+    list_a = sorted([int(x.split("   ")[0]) for x in lines])
+    list_b = sorted([int(x.split("   ")[1]) for x in lines])
+
+    counts_b = Counter(list_b)
+
+    weighted = [x * counts_b[x] for x in list_a]
+
+    print(sum(weighted))
+
+
 if __name__ == "__main__":
-    main()
+    step2()
