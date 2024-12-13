5eca26f21f2eacbd7ddd3d21ff715d79e1312234
Mike Franklin <mfranklin@sampleserve.com>
2024-12-12 23:43:29 -0600

    Day 13 Step 2 - Wrong

---
+++ src/aoc24/day13/step2.py
@@ -0,0 +1,56 @@
+from itertools import pairwise, tee
+from pathlib import Path
+from typing import List, Tuple, Dict
+import re
+
+LINE_REGEX = r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)"
+
+
+# a*ax + b*bx = px
+# a = (px - b*bx) / ax
+
+# a*ay + b*by = py
+# a = (py - b*by) / ay
+
+# (px - b*bx) / ax = (py - b*by) / ay
+# (px - b*bx)*ay = (py - b*by)*ax
+# px*ay - b*bx*ay = py*ax - b*by*ax
+# px*ay - py*ax = b*bx*ay  - b*by*ax
+# px*ay - py*ax = b * (bx*ay  - by*ax)
+# (px*ay - py*ax) / (bx*ay  - by*ax) = b
+
+
+def main():
+    file = Path(__file__).parent / "input"
+    # file = Path(__file__).parent / "example"
+    # file = Path(__file__).parent / "example_1"
+
+    data = file.read_text().strip()
+
+    total = 0
+
+    for case in re.findall(LINE_REGEX, data):
+        ax, ay, bx, by, px, py = map(int, case)
+
+        px += 10000000000000
+        py += 10000000000000
+
+        num = px * ay - py * ax
+        den = bx * ay - by * ax
+
+        b = num // den
+        if num % den != 0:
+            continue
+
+        print(ax, ay, bx, by, px, py, num, den, b)
+
+        a = (px - b * bx) // ax
+
+        total += 3 * a + b
+
+    assert total != 77779103984288
+    print("total", total)
+
+
+if __name__ == "__main__":
+    main()
