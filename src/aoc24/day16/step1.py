0d1f62457ecd30557927fe148aaa5185ec80ff7c
Mike Franklin <mfranklin@sampleserve.com>
2024-12-16 10:48:59 -0600

    Day 16 - Step 1 - Done - 105508

--- src/aoc24/day16/step1.py
+++ src/aoc24/day16/step1.py
@@ -61,7 +61,7 @@ def main():
     heap: List[Tuple[int, Tuple[int, int], List[Tuple[int, int]]]] = [
         (0, start, [start])
     ]
-    min_map: Dict[Tuple[int, int], int] = {}
+    min_map: Dict[Tuple[Tuple[int, int], Tuple[int, int]], int] = {}
 
     i = 0
     while True:
@@ -72,19 +72,18 @@ def main():
             print(i, score, len(heap))
             # draw(maze, path)
 
+        if pos == end:
+            draw(maze, path)
+            if expected is not None:
+                assert score_path(path) == expected, (
+                    score_path(path),
+                    expected,
+                )
+            print("Found", score_path(path))
+            return
+
         for d in DIRECTIONS:
             new_pos = (pos[0] + d[0], pos[1] + d[1])
-            if new_pos == end:
-                new_path = path + [new_pos]
-
-                draw(maze, new_path)
-                if expected is not None:
-                    assert score_path(new_path) == expected, (
-                        score_path(new_path),
-                        expected,
-                    )
-                print("Found", score_path(new_path))
-                return
             if maze[new_pos[1]][new_pos[0]] == "#":
                 continue
             if new_pos in path:
@@ -93,14 +92,15 @@ def main():
             new_path = path + [new_pos]
             new_score = score_path(new_path)
 
-            if (new_pos in min_map) and (new_score >= min_map[new_pos]):
+            key = (new_pos, d)
+            if (key in min_map) and (new_score >= min_map[key]):
                 continue
 
-            min_map[new_pos] = new_score
+            min_map[key] = new_score
 
             # Attempt 2
-            if new_score >= 105512:
-                continue
+            # if new_score >= 105512:
+            #    continue
 
             heapq.heappush(heap, (new_score, new_pos, new_path))
 
